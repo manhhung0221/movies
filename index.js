8576e6199f0c5f45cd38e037df5276f8611071b7
manhhung <hoangmanhhung.f@gmail.com>
2024-01-09 00:36:07 +0700

    242 video

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
@@ -5,24 +6,55 @@ const fetchData = async searchTerm => {
       s: searchTerm
     }
   });
-  if (response.data.Error){
+  if (response.data.Error) {
     return []
   }
   return response.data.Search
 };
+
+const root = document.querySelector(".autocomplete")
+root.innerHTML = `
+<label>Search For a Movie</label>
+<input class="input" />
+<div class="dropdown">
+  <div class="dropdown-menu">
+    <div class="dropdown-content results">
+    </div>
+  </div>
+</div>`
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector(".dropdown")
+const result = document.querySelector(".results")
+
 const onInput = async (event) => {
-  const movies= await fetchData(event.target.value);
-  for (movie of movies){
-    const movieElement=document.createElement("div")
-    if (movie.Poster!=='N/A'){
-      movieElement.innerHTML=`<h1>Title: ${movie.Title}</h1>
+  const movies = await fetchData(event.target.value);
+  dropdown.classList.add("is-active")
+  result.innerHTML = ""
+  if (!movies.length) {
+    dropdown.classList.remove("is-active")
+  }
+  for (let movie of movies) {
+    const option = document.createElement("a")
+
+    option.classList.add("dropdown-item")
+    if (movie.Poster !== 'N/A') {
+      option.innerHTML = `
       <img src="${movie.Poster}"/>
-      <br>`
-      document.body.appendChild(movieElement)
+      <h1>Title: ${movie.Title}</h1>`
+
+      result.appendChild(option)
     }
 
   }
 }
-input.addEventListener('input', debounce(onInput,500))
+input.addEventListener('input', debounce(onInput, 500))
+document.addEventListener("click",(event)=>{
+  if (!root.contains(event.target)){
+    dropdown.classList.remove("is-active")
+  }
+  else{
+    dropdown.classList.add("is-active")
+  }
+})
 
